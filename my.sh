661c5b0566746d6bf4b63be9fa5d217f8ae42c07
irakatz <40564069+irakatz@users.noreply.github.com>
2020-09-07 01:49:29 +0800

    Update my.sh

--- my.sh
+++ my.sh
@@ -1,11 +1,11 @@
 #!/bin/bash  
 
 starttime=$(date +%Y-%m-%d\ %H:%M:%S)
-echo $starttime
+echo $starttime >> ./result/benchshtest.txt
 
 for i in {1..10}  
 do  
 echo $i >> ./result/benchshtest.txt
 wget -qO- bench.sh | bash >> ./result/benchshtest.txt
-echo >> >> ./result/benchshtest.txt
+echo >> ./result/benchshtest.txt
 done  
