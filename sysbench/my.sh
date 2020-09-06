cfad3dddcdc687952ee45222a55cc11a94c3d4d5
irakatz <11711715@mail.sustech.edu.cn>
2020-09-06 16:32:21 +0800

    Update my.sh

--- sysbench/my.sh
+++ sysbench/my.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
-sysbench --num-threads=16 --test=cpu run >>./cpu/cputest.txt
-sysbench --num-threads=16 --test=fileio --file-test-mode=rndrw prepare
-sysbench --num-threads=16 --test=fileio --file-test-mode=rndrw run >>./fileio/fileiotest.txt
-sysbench --num-threads=16 --test=fileio --file-test-mode=rnd./rw cleanup
-sysbench --test=memory run >>./memory/memorytest.txt
+sysbench --num-threads=16 --max-requests=20000 --test=cpu run >> ./cpu/cputest.txt
+sysbench --num-threads=16 --test=fileio --file-total-size=3G --file-test-mode=rndrw prepare
+sysbench --num-threads=16 --test=fileio --file-total-size=3G --file-test-mode=rndrw run >> ./fileio/fileiotest.txt
+sysbench --num-threads=16 --test=fileio --file-total-size=3G --file-test-mode=rndrw cleanup
+sysbench --test=memory --memory-total-size=2048M --memory-oper=read run >> ./memory/memorytestread.txt
+sysbench --test=memory --memory-total-size=2048M --memory-oper=write run >> ./memory/memorytestwrite.txt
